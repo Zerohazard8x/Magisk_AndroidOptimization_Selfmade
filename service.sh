04145f8ae9b391a2660e704063d16b930cab9d45
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2023-08-05 22:10:18 +0800

    llm overhaul

--- service.sh
+++ service.sh
@@ -1,55 +1,48 @@
 #!/bin/bash
-cd $(dirname "$0")
+
+set -e
+cd "$(dirname "$0")"
 
 sleep 5
 chmod +x *
-sleep 5
 
-COUNT_FINAL=999
-until [[ $COUNT_FINAL == 0 ]]; do
+while true; do
     sleep 5
-    mount -o rw,exec,dev,remount /
-    mount -o rw,exec,dev,remount /sys
-    mount -o rw,exec,dev,remount /system
-
-    echo -n 0 >/sys/fs/selinux/enforce
-    setenforce 0
-
-    swapoff -a
-
-    #################################################################################
-    cat /system/build.prop
-    RESULT=$?
-    if [ $RESULT == 0 ]; then
-        if [[ $(cat /system/build.prop | egrep -i persist.camera.HAL3.enabled) != $null ]]; then
-            sed -i 's/persist.camera.HAL3.enabled=0/persist.camera.HAL3.enabled=1/g' /system/build.prop
-        else
-            echo "persist.camera.HAL3.enabled=1" >>/system/build.prop
-        fi
-
-        if [[ $(cat /system/build.prop | egrep -i sys.use_fifo_ui) != $null ]]; then
-            sed -i 's/sys.use_fifo_ui=0/sys.use_fifo_ui=1/g' /system/build.prop
-        else
-            echo "sys.use_fifo_ui=1" >>/system/build.prop
-        fi
-
-        if [[ $(cat /system/build.prop | egrep -i wlan.wfd.hdcp) != $null ]]; then
-            sed -i 's/wlan.wfd.hdcp=enable/wlan.wfd.hdcp=disable/g' /system/build.prop
-        else
-            echo "wlan.wfd.hdcp=disable" >>/system/build.prop
-        fi
-    fi
-
-    echo performance >/sys/kernel/gpu/gpu_governor
-    #################################################################################
-    for cpu_folder in $(find /sys/devices/system/cpu/cpu* -type d -maxdepth 0 | sort); do
-        echo ondemand >"$cpu_folder"/cpufreq/scaling_governor
-        echo performance >"$cpu_folder"/cpufreq/scaling_governor
+    
+    # Remount necessary partitions
+    for mount_point in / /sys /system; do
+        mount -o remount,rw,exec,dev "$mount_point"
     done
+    
+    # Update build.prop settings
+    if grep -Eqi "persist.camera.HAL3.enabled" /system/build.prop; then
+        sed -i 's/persist.camera.HAL3.enabled=0/persist.camera.HAL3.enabled=1/g' /system/build.prop
+    else
+        echo "persist.camera.HAL3.enabled=1" >> /system/build.prop
+    fi
+    
+    # grep -Eqi "sys.use_fifo_ui" /system/build.prop; then
+    #     sed -i 's/sys.use_fifo_ui=0/sys.use_fifo_ui=1/g' /system/build.prop
+    # else
+    #     echo "sys.use_fifo_ui=1" >>/system/build.prop
+    # fi
+    
+    if grep -Eqi "wlan.wfd.hdcp" /system/build.prop; then
+        sed -i 's/wlan.wfd.hdcp=enable/wlan.wfd.hdcp=disable/g' /system/build.prop
+    else
+        echo "wlan.wfd.hdcp=disable" >> /system/build.prop
+    fi
+    
+    # Set GPU governor and CPU scaling governor
+    echo "performance" > /sys/kernel/gpu/gpu_governor
+    for cpu_folder in /sys/devices/system/cpu/cpu*/cpufreq; do
+        echo "performance" > "$cpu_folder/scaling_governor"
+    done
+    
+    # Restart ADB
     setprop service.adb.tcp.port 5555
     stop adbd
     start adbd
-    ((COUNT_FINAL = COUNT_FINAL - 1))
-    ((COUNT_FINAL = COUNT_FINAL + 1))
+    
     sleep 50000
 done
